2a61c5f83ad5e363d11d1f1c9661c61fe3ad3b3b
Terry Geng <gengyanda@gmail.com>
2020-10-29 18:50:11 +0800

    feat: add scroll bar to terminal.

--- mamba_client/widgets/terminal.py
+++ mamba_client/widgets/terminal.py
@@ -1,20 +1,33 @@
 import MambaICE.Dashboard as Dashboard
 
 from termqt import Terminal
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QScrollBar
+from PyQt5.QtCore import Qt
 
 import mamba_client
 
 
-class TerminalWidget(Terminal, Dashboard.TerminalClient):
+class TerminalWidget(QWidget, Dashboard.TerminalClient):
     def __init__(self, communicator, terminal_host, logger):
-        super().__init__(400, 250, logger=logger)
+        super().__init__()
+        self.terminal_widget = Terminal(400, 250, logger=logger)
 
         self.communicator = communicator
         self.host = terminal_host
         self.register_client_instance()
 
-        self.stdin_callback = self.host.stdin
-        self.resize_callback = self.host.resize
+        self.terminal_widget.stdin_callback = self.host.stdin
+        self.terminal_widget.resize_callback = self.host.resize
+
+        self.scroll_bar = QScrollBar(Qt.Vertical, self)
+        self.terminal_widget.connect_scroll_bar(self.scroll_bar)
+
+        self.layout = QHBoxLayout()
+        self.layout.setSpacing(0)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.addWidget(self.terminal_widget)
+        self.layout.addWidget(self.scroll_bar)
+        self.setLayout(self.layout)
 
         self.resize(400, 250)
 
@@ -30,7 +43,7 @@ class TerminalWidget(Terminal, Dashboard.TerminalClient):
         self.host.registerClient(proxy)
 
     def stdout(self, s, current):
-        super().stdout(s)
+        self.terminal_widget.stdout(s)
 
     @classmethod
     def get_init_func(cls, communciator, terminal_host, logger):
